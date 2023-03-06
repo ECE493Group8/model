0cc6694633fd6f4188654e6eb81096a7a5b55521
Christian Jans <cjjans@ualberta.ca>
2023-03-06 23:51:43 +0000

    Add initial setup instructions

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Model
 
 This repository contains the code to train models.
+
+# Installation
+
+```sh
+$ python3 -m venv .venv
+$ source .venv/bin/activate
+$ pip install -e .
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup_info = dict(
+    name="model",
+    version=1.0,
+    packages=find_packages(),
+    install_requires=[
+        "gensim",
+    ],
+)
+
+setup(**setup_info)
