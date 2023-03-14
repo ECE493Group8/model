d6fc929765d85c84f0affd698be5fa331dde5c6d
Christian Jans <cjjans@ualberta.ca>
2023-03-14 03:30:45 +0000

    Flesh out the objects more

--- src/models/word2vec.py
+++ src/models/word2vec.py
@@ -1,55 +1,64 @@
-from gensim.models import Word2Vec as GensimWord2Vec
-from gensim.test.utils import common_texts
+from typing import List, Iterable
 
-from utils.callbacks import Word2VecEpochCallback
+from gensim.models import Word2Vec
+from gensim.models.callbacks import CallbackAny2Vec
 
 
-class Word2Vec:
-    def __init__(self):
-        self.epoch_callback = Word2VecEpochCallback()
-        self.model = GensimWord2Vec(sentences=common_texts,
-                                    corpus_file=None,
-                                    vector_size=16,
-                                    window=5,
-                                    min_count=1,  # Ignore all words with total frequency lower than this
-                                    workers=1,
-                                    sg=1,  # 0 for skip-gram, 1 for CBOW
-                                    hs=0,  # 0 for negative sampling, 1 for hierarchical softmax
-                                    negative=5,  # Negative sampling used if > 0, specifies how many noise words should be drawn (usually between 5 and 20), 0 if no noise words
-                                    ns_exponent=0.75,  # Exponent used to shape negative sampling distribution (popular default of 0.75)
-                                    cbow_mean=0,  # 0 to use sum of context word vectors, 1 to use mean (only applies when CBOW is used)
-                                    alpha=0.025,  # Initial learning rate
-                                    seed=0,  # Seed for RNG
-                                    max_vocab_size=None,  # Limits RAM during vocabulary building (None for no limit)
-                                    max_final_vocab=None,  # Limits the vocab to a target vocab size
-                                    sample=0.001,  # TODO: This might not be a good value
-                                    hashfxn=None,
-                                    epochs=1000,
-                                    trim_rule=None,
-                                    sorted_vocab=0,  # If 1, sort the vocabulary by descending frequency before assigning word indexes
-                                    batch_words=10000,  # Target size (in words) for batches of examples passed to worker threads
-                                    compute_loss=True,
-                                    callbacks=[self.epoch_callback],
-                                    shrink_windows=None)
+class MalamudWord2Vec(Word2Vec):
+    MAX_NGRAM_SIZE = 5
+    MIN_NGRAM_SIZE = 1
+
+    def __init__(self,
+                 workers: int,
+                 epochs: int,
+                 vector_size: int):
+        super().__init__(vector_size=vector_size,
+                         window=MalamudWord2Vec.MAX_NGRAM_SIZE,
+                         min_count=MalamudWord2Vec.MIN_NGRAM_SIZE,  # Ignore all words with total frequency lower than this
+                         workers=workers,
+                         sg=1,  # 0 for skip-gram, 1 for CBOW
+                         hs=0,  # 0 for negative sampling, 1 for hierarchical softmax
+                         negative=5,  # Negative sampling used if > 0, specifies how many noise words should be drawn (usually between 5 and 20), 0 if no noise words
+                         ns_exponent=0.75,  # Exponent used to shape negative sampling distribution (popular default of 0.75)
+                         cbow_mean=0,  # 0 to use sum of context word vectors, 1 to use mean (only applies when CBOW is used)
+                         alpha=0.025,  # Initial learning rate
+                         min_alpha=0.0001,
+                         max_vocab_size=None,  # Limits RAM during vocabulary building (None for no limit)
+                         max_final_vocab=None,  # Limits the vocab to a target vocab size
+                         sample=1e-3,  # TODO: This might not be a good value
+                         hashfxn=hash,
+                         epochs=epochs,
+                         trim_rule=None,  # TODO: Look into if we need this. This specifies whether certain words should remain in the vocabulary, be trimmed away, or handled using the default (None => discarded if word count < min_count).
+                         sorted_vocab=0,  # TODO: It's probably best if this is 0? If 1, sort the vocabulary by descending frequency before assigning word indexes
+                        #  batch_words=10000,  # Target size (in words) for batches of examples passed to worker threads
+                         shrink_windows=True,  # TODO: A variably-sized window, is this what we want?
+                         null_word=0,
+                         seed=1)
         
-        self.model.build_vocab()
 
-        self.model.save("word2vec.model")
+    def train(self,
+              dataset: Iterable,
+              callbacks: List[CallbackAny2Vec],
+              report_delay: float):
+            #   save_frequency: float):
+        # self.save_counter = 0
+        # self.save_every = int(save_frequency // report_delay)
+        # assert self.save_every >= 1
 
-    def train(self):
-        pass
+        super().train(corpus_iterable=dataset,
+                      epochs=self.epochs,
+                      total_examples=self.corpus_count,
+                      compute_loss=True,
+                      callbacks=callbacks,
+                      report_delay=report_delay)
 
-    # TODO
-    # @classmethod
-    # def load(cls, path: str):
-    #     self.model = GensimWord2Vec.load(path)
-
-    def save(self, path: str):
-        pass
-
-
-if __name__ == "__main__":
-    for text in common_texts:
-        print(text)
-    w2v = Word2Vec()
-
+    def _log_progress(self, job_queue, progress_queue, cur_epoch, example_count,
+                      total_examples, raw_word_count, total_words,
+                      trained_word_count, elapsed):
+        # self.save_counter += 1
+        # if self.save_counter % self.save_every == 0:
+        #     self.save()
+        super()._log_progress(job_queue, progress_queue, cur_epoch,
+                              example_count, total_examples, raw_word_count,
+                              total_words, trained_word_count, elapsed)
+        
