6e8494ccf2b4ace3778ca811caa49bd801c4c785
Justin Boileau <justin@blindnet.io>
2023-03-19 21:35:46 -0600

    use ngram_lc column instead

--- src/data/malamud_dataset.py
+++ src/data/malamud_dataset.py
@@ -4,7 +4,7 @@ import connectorx as cx
 class MalamudDataset:
     def __init__(self, postgres_uri: str):
         self.df = cx.read_sql(postgres_uri, "SELECT * FROM docs.doc_ngrams_0")
-        self.ngrams = self.df.ngram.apply(gensim.utils.simple_preprocess)
+        self.ngrams = self.df.ngram_lc.apply(gensim.utils.simple_preprocess)
 
     def __iter__(self):
         for ngram in self.ngrams:
