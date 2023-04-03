2414ea35c0956ed9ec4f6c7c811a7b584d2ee2a6
Christian Jans <cjjans@ualberta.ca>
2023-04-03 23:49:11 +0000

    Use glob to find parquet files

--- src/data/malamud_dataset.py
+++ src/data/malamud_dataset.py
@@ -1,7 +1,7 @@
-import pandas as pd
 import polars as pl
 import itertools
 import argparse
+import glob
 from time import perf_counter
 
 class MalamudDataset:
@@ -14,20 +14,17 @@ class MalamudDataset:
         num_files: Integer indicating the number of files to load, will replace <X> in parq_path
         column: String indicating column to use within the selected table
     """
-    def __init__(self, parq_base_path: str, num_files: int, column: str):
+    def __init__(self, parquet_path: str, column: str):
         
         # Save params
-        self.parq_base_path = parq_base_path
-        self.num_files = num_files
+        self.parquet_path = parquet_path
         self.column = column
 
         # Load each df into memory
         self.dfs = []
-        for i in range(1, self.num_files+1):
-
+        for parquet_file in glob.glob(parquet_path):
             # Get the next keyword file name
-            parq_path = self.parq_base_path.replace('<X>', str(i))
-            self.dfs.append(pl.scan_parquet(parq_path).select(column).collect())
+            self.dfs.append(pl.scan_parquet(parquet_file).select(column).collect())
 
     def __iter__(self):
         """Yield a value from each dataframe"""
@@ -42,7 +39,7 @@ if __name__ == "__main__":
     """
     If calling this file directly from the command line, print time to read all files
 
-    Example usage: "python src/data/malamud_dataset.py --parq_base_path "docs.doc_ngram_0_<X>.parquet" --num_files 16 --column ngram_lc"
+    Example usage: "python src/data/malamud_dataset.py --parquet_path "*.parquet" --column ngram_lc"
     
     Benchmarks:
         For 650k row csv split into 16 chunks and converted to parquet:
@@ -51,13 +48,12 @@ if __name__ == "__main__":
     """
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--parq_base_path", type=str, required=True)
-    parser.add_argument("--num_files", type=int, required=True)
+    parser.add_argument("--parquet_path", type=str, required=True)
     parser.add_argument("--column", type=str, required=True)
     parser.add_argument("--print", action="store_true", default=False)
     args = parser.parse_args()
     t = perf_counter()
-    dataset = MalamudDataset(args.parq_base_path, args.num_files, args.column)
+    dataset = MalamudDataset(args.parquet_path, args.column)
     for r in dataset:
         if args.print:
             print(r)
