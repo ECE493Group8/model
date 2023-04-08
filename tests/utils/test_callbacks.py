3cfa84af4d8d5dd4ff21ddd23f197d55e846a030
Christian Jans <cjjans@ualberta.ca>
2023-04-08 14:55:35 +0000

    Use mocking for epoch callback tests

--- tests/utils/test_callbacks.py
+++ tests/utils/test_callbacks.py
@@ -1,48 +1,84 @@
-import logging
 import os
-import shutil
 import unittest
+import unittest.mock
 
 from gensim.models import Word2Vec
-from gensim.test.utils import common_texts
 
 from utils.callbacks import EpochLogger, EpochSaver
 
 
-class CallbacksTest(unittest.TestCase):
-    TEST_DIRECTORY = "./tests/callbacks_test"
+class EpochSaverTest(unittest.TestCase):
+    TEST_DIRECTORY = "./tests/epoch_saver_test"
+    TRAINING_LOSS = 0.5
+    SAVE_FREQUENCY = 2
 
     def setUp(self) -> None:
-        os.makedirs(CallbacksTest.TEST_DIRECTORY)
+        if not os.path.exists(EpochSaverTest.TEST_DIRECTORY):
+            os.makedirs(EpochSaverTest.TEST_DIRECTORY)
 
-    def test_epoch_callback(self):
-        logging.basicConfig(
-                filename=os.path.join(CallbacksTest.TEST_DIRECTORY, "test.log"),
-                level=logging.INFO)
+        self.epoch_saver = EpochSaver(EpochSaverTest.TEST_DIRECTORY,
+                                      EpochSaverTest.SAVE_FREQUENCY)
 
-        epoch_saver = EpochSaver(CallbacksTest.TEST_DIRECTORY, 1)
-        epoch_logger = EpochLogger()
+        self.model_mock = Word2Vec()
+        self.model_mock.save = unittest.mock.MagicMock()
+        self.model_mock.get_latest_training_loss = \
+            unittest.mock.MagicMock(return_value=EpochSaverTest.TRAINING_LOSS)
 
-        model = Word2Vec(window=5,
-                         min_count=1,
-                         workers=4,
-                         epochs=10,
-                         compute_loss=True,
-                         seed=1)  # Runnning with seed=1 ensures that a best model will be saved.
-        model.build_vocab(common_texts, progress_per=1)
-        model.train(
-                common_texts, total_examples=model.corpus_count,
-                epochs=model.epochs, compute_loss=True,
-                callbacks=[epoch_logger, epoch_saver])
+    def test_epoch_saver_on_train_begin(self):
+        self.epoch_saver.on_train_begin(self.model_mock)
+        self.model_mock.save.assert_called_once_with(
+            os.path.join(EpochSaverTest.TEST_DIRECTORY, "start.model"))
 
-        self.assertTrue(os.path.exists(CallbacksTest.TEST_DIRECTORY))
-        self.assertTrue(os.path.exists(os.path.join(CallbacksTest.TEST_DIRECTORY, f"start.model")))
-        self.assertTrue(os.path.exists(os.path.join(CallbacksTest.TEST_DIRECTORY, f"best.model")))
-        for i in range(1, 10 + 1):
-            self.assertTrue(os.path.exists(os.path.join(CallbacksTest.TEST_DIRECTORY, f"model_{i}.model")))
+    def test_epoch_saver_on_epoch_begin(self):
+        last_epoch = self.epoch_saver.epoch
+        self.epoch_saver.on_epoch_begin(self.model_mock)
+        this_epoch = self.epoch_saver.epoch
+        self.assertEqual(this_epoch, last_epoch + 1)
 
-    def tearDown(self) -> None:
-        shutil.rmtree(CallbacksTest.TEST_DIRECTORY)
+    def test_epoch_saver_on_epoch_end(self):
+        self.assertEqual(self.epoch_saver.best_loss, float("inf"))
+        self.assertEqual(self.epoch_saver.loss, 0.)
+
+        self.epoch_saver.on_epoch_end(self.model_mock)
+        self.model_mock.save.has_calls([
+            unittest.mock.call(os.path.join(EpochSaverTest.TEST_DIRECTORY, "model_0.epoch")),
+            unittest.mock.call(os.path.join(EpochSaverTest.TEST_DIRECTORY, "best.model")),
+        ])
+        self.epoch_saver.on_epoch_end(self.model_mock)  # No new best loss and no frequent save.
+        self.model_mock.save.has_calls([
+            unittest.mock.call(os.path.join(EpochSaverTest.TEST_DIRECTORY, "model_0.epoch")),
+            unittest.mock.call(os.path.join(EpochSaverTest.TEST_DIRECTORY, "best.model")),
+        ])
+        self.epoch_saver.on_epoch_end(self.model_mock)  # A frequent save.
+        self.model_mock.save.has_calls([
+            unittest.mock.call(os.path.join(EpochSaverTest.TEST_DIRECTORY, "model_0.epoch")),
+            unittest.mock.call(os.path.join(EpochSaverTest.TEST_DIRECTORY, "best.model")),
+            unittest.mock.call(os.path.join(EpochSaverTest.TEST_DIRECTORY, "model_2.epoch")),
+        ])
+
+
+class EpochLoggerTest(unittest.TestCase):
+    TRAINING_LOSS = 0.5
+
+    def setUp(self) -> None:
+        self.epoch_logger = EpochLogger()
+
+        self.model_mock = Word2Vec()
+        self.model_mock.save = unittest.mock.MagicMock()
+        self.model_mock.get_latest_training_loss = \
+            unittest.mock.MagicMock(return_value=EpochLoggerTest.TRAINING_LOSS)
+
+    def test_epoch_logger_on_epoch_begin(self):
+        last_epoch = self.epoch_logger.epoch
+        self.epoch_logger.on_epoch_begin(self.model_mock)
+        this_epoch = self.epoch_logger.epoch
+        self.assertEqual(this_epoch, last_epoch + 1)
+
+    def test_epoch_logger_on_epoch_end(self):
+        self.assertEqual(self.epoch_logger.loss, 0.)
+        self.epoch_logger.on_epoch_end(self.model_mock)
+        self.model_mock.get_latest_training_loss.assert_called_once()
+        self.assertEqual(self.epoch_logger.loss, EpochLoggerTest.TRAINING_LOSS)
 
 
 if __name__ == "__main__":
